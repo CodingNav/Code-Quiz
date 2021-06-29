dfd432b5afbfa72ff54c248b69e5a2f536610cf1
CodingNav <navleenkaurr@gmail.com>
2021-06-29 12:15:29 -0400

    Created variables to target different ids

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var startPage = document.querySelector("#start-page");
+var startButton = document.querySelector("#start-button");
+var questionPage = document.querySelector("#question-page");
+var endPage = document.querySelector("#end-page");
+var initialButton = document.querySelector("initial-button");
+
+
+
