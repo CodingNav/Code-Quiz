41e68640e05cef8e43267af31e101fd33f35e606
CodingNav <navleenkaurr@gmail.com>
2021-06-29 12:38:30 -0400

    Added an array for the questions, choices, correct answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,69 @@ var questionPage = document.querySelector("#question-page");
 var endPage = document.querySelector("#end-page");
 var initialButton = document.querySelector("initial-button");
 
+var questions = [
+    {
+        question: "What?",
+        choices: ["A", "B", "C", "D"],
+        correctAnswer: "B"
+    },
+    {
+        question: "What?",
+        choices: ["A", "B", "C", "D"],
+        correctAnswer: "B"
+    },
+    {
+        question: "What?",
+        choices: ["A", "B", "C", "D"],
+        correctAnswer: "B"
+    },
+    {
+        question: "What?",
+        choices: ["A", "B", "C", "D"],
+        correctAnswer: "B"
+    },
+    {
+        question: "What?",
+        choices: ["A", "B", "C", "D"],
+        correctAnswer: "B"
+    },
+    {
+        question: "What?",
+        choices: ["A", "B", "C", "D"],
+        correctAnswer: "B"
+    },
+    {
+        question: "What?",
+        choices: ["A", "B", "C", "D"],
+        correctAnswer: "B"
+    },
+    {
+        question: "What?",
+        choices: ["A", "B", "C", "D"],
+        correctAnswer: "B"
+    },
+    {
+        question: "What?",
+        choices: ["A", "B", "C", "D"],
+        correctAnswer: "B"
+    },
+    {
+        question: "What?",
+        choices: ["A", "B", "C", "D"],
+        correctAnswer: "B"
+    },
+]
+
 questionPage.style.display = 'none';
 endPage.style.display = 'none';
 
 
-startButton.addEventListener('click', function() { 
+startButton.addEventListener('click', function () {
     startPage.style.display = 'none';
     questionPage.style.display = 'block';
 });
+
+
+
+
+
